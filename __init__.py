8d8fe1b9fa7a407db293208899a6a775e26ddc3c
Abou Ly <166803585+abou94190@users.noreply.github.com>
2024-05-23 14:53:55 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -76,6 +76,15 @@ def enregistrer_client():
     conn.commit()
     conn.close()
     return redirect('/consultation/')  # Rediriger vers la page d'accueil après l'enregistrement
-                                                                                                                                       
+
+@app.route('/fiche_nom/')
+def fiche_nom():
+    if request.authorization and request.authorization.username == 'user' and request.authorization.password == '12345':
+        # Si l'utilisateur est authentifié avec les bons identifiants
+        return "<h2>Bienvenue sur la page fiche_nom</h2>"
+    else:
+        # Si les identifiants sont incorrects, demander l'authentification
+        return render_template('authentification_required.html'), 401
+
 if __name__ == "__main__":
   app.run(debug=True)
